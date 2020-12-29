bdb6e16035aaa565d13d63cfb3ea8093b3f5df10
Konstantin S <HavenDV@users.noreply.github.com>
2020-12-29 19:59:26 +1000

    Added YandexSearcher to IntegrationTests.

--- src/tests/H.Runners.TorrentRunner.IntegrationTests/SimpleTests.cs
+++ src/tests/H.Runners.TorrentRunner.IntegrationTests/SimpleTests.cs
@@ -1,8 +1,10 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using H.Core;
+using H.Searchers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace H.Runners.IntegrationTests
@@ -27,10 +29,16 @@ namespace H.Runners.IntegrationTests
                 Assert.Inconclusive("QBitTorrent is not installed.");
             }
             
-            runner.CommandReceived += (_, value) => Console.WriteLine($"{value}");
-            // Search command.
-            runner.AsyncCommandReceived += (_, _, _) => Task.FromResult<IValue>(new Value(
-                "https://kinotorrs.me/melodrama/krasotka_1990_skachat_torrent"));
+            runner.CommandReceived += (_, command) => Console.WriteLine($"{command}");
+            runner.AsyncCommandReceived += async (_, command, token) =>
+            {
+                Console.WriteLine($"{command}");
+
+                using var searcher = new YandexSearcher();
+                var results = await searcher.SearchAsync(command.Input.Argument, token);
+
+                return new Value(results.Select(result => result.Url).ToArray());
+            };
             
             await runner.TorrentAsync("красотка", cancellationToken);
         }
