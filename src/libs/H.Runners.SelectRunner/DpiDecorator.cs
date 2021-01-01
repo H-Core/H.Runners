f4e9b02da9f3b55751f56a1681cf9ceb74a8a2b6
Konstantin S <HavenDV@users.noreply.github.com>
2021-01-02 02:26:56 +1000

    Added DpiDecorator.

---
+++ src/libs/H.Runners.SelectRunner/DpiDecorator.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace H.Runners
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DpiDecorator : Decorator
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public DpiDecorator()
+        {
+            this.Loaded += (s, e) =>
+            {
+                Matrix m = PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice;
+                ScaleTransform dpiTransform = new ScaleTransform(1 / m.M11, 1 / m.M22);
+                if (dpiTransform.CanFreeze)
+                    dpiTransform.Freeze();
+                this.LayoutTransform = dpiTransform;
+            };
+        }
+    };
+};
