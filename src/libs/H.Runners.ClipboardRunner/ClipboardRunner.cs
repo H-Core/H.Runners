b46ec6189fba59510e31a785e7fc3681725597bc
Konstantin S <HavenDV@users.noreply.github.com>
2021-01-06 13:10:40 +1000

    Added new commands to ClipboardRunner.

--- src/libs/H.Runners.ClipboardRunner/ClipboardRunner.cs
+++ src/libs/H.Runners.ClipboardRunner/ClipboardRunner.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using H.Core;
 using H.Core.Runners;
 
 namespace H.Runners
@@ -27,6 +28,24 @@ namespace H.Runners
             Add(new AsyncAction("clipboard-set", async (command, cancellationToken) =>
             {
                 await SetClipboardTextAsync(command.Input.Argument, cancellationToken).ConfigureAwait(false);
+
+                return command.Input;
+            }));
+            Add(new AsyncAction("clipboard-get", async (_, cancellationToken) =>
+            {
+                var text = await GetClipboardTextAsync(cancellationToken).ConfigureAwait(false);
+
+                return new Value(text);
+            }));
+            Add(new AsyncAction("copy", async (_, cancellationToken) =>
+            {
+                var text = await CopyAsync(cancellationToken).ConfigureAwait(false);
+
+                return new Value(text);
+            }));
+            Add(new AsyncAction("paste", async (command, cancellationToken) =>
+            {
+                await PasteAsync(command.Input.Argument, cancellationToken).ConfigureAwait(false);
 
                 return command.Input;
             }));
@@ -76,7 +95,39 @@ namespace H.Runners
             }
             Application = Application ?? throw new InvalidOperationException("Application is null.");
 
-            Application.Dispatcher?.Invoke(() => Clipboard.SetText(text, TextDataFormat.Text));
+            Application.Dispatcher?.Invoke(() => Clipboard.SetText(text));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        public async Task<string> GetClipboardTextAsync(CancellationToken cancellationToken = default)
+        {
+            if (Application == null)
+            {
+                await InitializeAsync(cancellationToken).ConfigureAwait(false);
+            }
+            Application = Application ?? throw new InvalidOperationException("Application is null.");
+
+            var text = Application.Dispatcher?.Invoke(Clipboard.GetText);
+
+            return text ?? string.Empty;
+        }
+
+        private async Task<string> CopyAsync(CancellationToken cancellationToken = default)
+        {
+            await RunAsync(new Command("keyboard", "Control+C"), cancellationToken).ConfigureAwait(false);
+
+            return await GetClipboardTextAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        private async Task PasteAsync(string text, CancellationToken cancellationToken = default)
+        {
+            text = text ?? throw new ArgumentNullException(nameof(text));
+
+            await SetClipboardTextAsync(text, cancellationToken).ConfigureAwait(false);
+            await RunAsync(new Command("keyboard", "Control+V"), cancellationToken).ConfigureAwait(false);
         }
 
         #endregion
