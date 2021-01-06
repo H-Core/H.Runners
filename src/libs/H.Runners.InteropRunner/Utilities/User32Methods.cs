8bf31e9f18e70ad8710266deb55cca26a5b7fd6b
Konstantin S <HavenDV@users.noreply.github.com>
2021-01-06 14:00:50 +1000

    Added InteropRunner.

---
+++ src/libs/H.Runners.InteropRunner/Utilities/User32Methods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace H.Runners.Utilities
+{
+    internal static class User32Methods
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Rect
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDesktopWindow();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetShellWindow();
+
+        [DllImport("user32.dll")]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, [In, Out] ref Rect rect);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern bool ShowWindow(HandleRef hwnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(HandleRef hwnd);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetFocus(HandleRef hwnd);
+
+    }
+}
---
+++ src/libs/H.Runners.InteropRunner/Utilities/User32Utilities.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace H.Runners.Utilities
+{
+    internal static class User32Utilities
+    {
+        public static int GetWindowProcessId(IntPtr hWnd)
+        {
+            var _ = User32Methods.GetWindowThreadProcessId(hWnd, out var pid);
+
+            return pid;
+        }
+
+        public static User32Methods.Rect GetWindowRect(IntPtr hWnd)
+        {
+            var rect = new User32Methods.Rect();
+            User32Methods.GetWindowRect(hWnd, ref rect);
+
+            return rect;
+        }
+
+        public static Process GetForegroundProcess()
+        {
+            var hWnd = User32Methods.GetForegroundWindow();
+            var id = GetWindowProcessId(hWnd);
+
+            return Process.GetProcessById(id);
+        }
+    }
+}
