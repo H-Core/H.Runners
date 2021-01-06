390a9617a00f55e7bb71f5c8b406bb6553b9fa15
Konstantin S <HavenDV@users.noreply.github.com>
2021-01-06 14:08:26 +1000

    Refactored.

---
+++ src/libs/H.Runners.ClipboardRunner/NotifyRunner.cs
@@ -0,0 +1,39 @@
+﻿using System.Media;
+using H.Core.Runners;
+using H.Runners.Utilities;
+
+namespace H.Runners
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class NotifyRunner : Runner
+    {
+        #region Constructors
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public NotifyRunner()
+        {
+            Add(new SyncAction("notify", Notify));
+        }
+
+        #endregion
+
+        #region Private methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Notify()
+        {
+            using var stream = ResourcesUtilities.ReadFileAsStream("beep.wav");
+            using var player = new SoundPlayer(stream);
+
+            player.Play();
+        }
+
+        #endregion
+    }
+}
